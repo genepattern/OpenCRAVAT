0051d4d2c947c97043849a7d2a66fe6ac0836e1f
ted Liefeld <liefeld@broadinstitute.org>
2020-03-26 09:30:05 -0700

    Updates to metadata and added mapping between display and parameter names for the annotation tools. Better handle errors on the CRAVAT server

--- GpUnit/runLocal.sh
+++ GpUnit/runLocal.sh
@@ -1,4 +1,4 @@
 
 rm -rf job_1/*
 
-docker run  -v ${PWD}:$PWD -w $PWD/job_1 genepattern/opencravat python /opencravat/opencravat_command_line.py -i $PWD/data/example_input -user $1 -pass $2 -ann1 clinvar,dbsnp 
+docker run  -v ${PWD}:$PWD -w $PWD/job_1 genepattern/opencravat python /opencravat/opencravat_command_line.py -i $PWD/data/example_input -user $1 -pass $2 -ann1 clinvar,dbsnp  -assembly hg38
--- opencravat_command_line.py
+++ opencravat_command_line.py
@@ -17,57 +17,65 @@ from requests.auth import HTTPBasicAuth
 
 print('- OpenCravat Module - remote API -',flush=True)
     
-def run_opencravat(input_file='example_input', username='', password='', annotators='["clinvar", "ncbigene"]', base_url='https://run.opencravat.org'):
+def run_opencravat(input_file='example_input', username='', password='', assembly="hg38", annotators='["clinvar", "ncbigene"]', base_url='https://run.opencravat.org'):
     login_url = f'{base_url}/server/login'
     submit_url = f'{base_url}/submit/submit'
     session = requests.Session()
     print(f'Logging in...')
     print("annotating with "+ annotators, flush=True)
+    print("assembly is " + assembly)
     s = session.get(login_url, auth=HTTPBasicAuth(username, password))
     print("Login succesful... trying " + submit_url)
+    annotators = "clinvar,dbsnp"
+    s = session.post(submit_url, files={'file_0': open(input_file)}, data={'options': '{"annotators": ["clinvar"], "reports": ["text"], "assembly": "'+assembly+'", "note": "Run from GenePatern"}'})
+    #s = session.post(submit_url, files={'file_0': open(input_file)}, data={'options': '{"annotators": ["clinvar", "dbsnp", "ncbigene"], "reports": ["text"], "assembly": "'+assembly+'", "note": "test run"}'})
+    
 
-    s = session.post(submit_url, files={'file_0': open(input_file)}, data={'options': '{"annotators": ['+annotators+'], "reports": ["text"], "assembly": "hg38", "note": "test run"}'})
     print("job submitted")
     jobid = s.json()['id']
     print(f'Job submitted. Job ID={jobid}')
     status_url = f'{base_url}/submit/jobs/{jobid}/status'
-
+    status = 'starting'
     while True:
         r = session.get(status_url)
-        if r.json()['status'] != 'Finished':
-            #uio.status = 'Running'
+        status = r.json()['status']
+        if (status != 'Finished') & (status != 'Error'):
+            print(r.json()['status'])
             print(f'Waiting for the job to finish...', flush=True)
             time.sleep(5)
         else:
             break
 
-    report_url = f'{base_url}/submit/jobs/{jobid}/reports/tsv'
-    report_list_url = f'{base_url}/submit/jobs/{jobid}/reports'
-    s = session.get(report_list_url)
-    if 'tsv' not in s.json():
-        #uio.status = 'Generating Report'
-        print(f'Generating a TSV report...')
-        s = session.post(report_url)
-        if s.json() == 'done':
-            #uio.status = 'Success'
-            print(f'TSV report generation is done.')
-    s = session.get(report_url, stream=True)
-    wf = open(f'{jobid}.tsv.zip', 'wb')
-    shutil.copyfileobj(s.raw, wf)
-    del s
-    wf.close()
-    # https://run.opencravat.org/result/index.html?job_id=
-    index  = open(f'index.html', 'w')
-    index.write("<html><head><meta http-equiv=\"refresh\" content=\"0; url=https://run.opencravat.org/result/index.html?job_id="+jobid+"\"/></head></html>")
-    index.close()
-
+    if (status == 'Finished'):
+        report_url = f'{base_url}/submit/jobs/{jobid}/reports/tsv'
+        report_list_url = f'{base_url}/submit/jobs/{jobid}/reports'
+        s = session.get(report_list_url)
+        if 'tsv' not in s.json():
+            #uio.status = 'Generating Report'
+            print(f'Generating a TSV report...')
+            s = session.post(report_url)
+            if s.json() == 'done':
+                #uio.status = 'Success'
+                print(f'TSV report generation is done.')
+        s = session.get(report_url, stream=True)
+        wf = open(f'{jobid}.tsv.zip', 'wb')
+        shutil.copyfileobj(s.raw, wf)
+        del s
+        wf.close()
+        # https://run.opencravat.org/result/index.html?job_id=
+        index  = open(f'{jobid}.html', 'w')
+        index.write("<html><head><meta http-equiv=\"refresh\" content=\"0; url=https://run.opencravat.org/result/index.html?job_id="+jobid+"\"/></head></html>")
+        index.close()
+    else:
+        print("An error occurred.  See the response from the OpenCravat server.")
+        raise SystemExit(r.json())
 
 def main():
     parser = argparse.ArgumentParser(description='%s Parameters' % __tool_name__ ,formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("-i", "--input-file", dest="input_filename",default = None, help="input file name", metavar="FILE")
     parser.add_argument("-user",dest="user",  default='',  help="OpenCravat account User name")
     parser.add_argument("-pass",dest="ocpassword", default='',  help="OpenCravat account password")   
-
+    parser.add_argument("-assembly",dest="assembly",  default='hg38',  help="genome Assemply to use")
     for x in range(1,21):
         parser.add_argument("-ann"+str(x), "--annotators"+str(x), dest="ann"+str(x),default = '', help="annotator ")
 
@@ -95,7 +103,7 @@ def main():
     annString = ",".join(list(m))
     print("AnnString is " + annString)
 
-    run_opencravat(input_file=args.input_filename, username=args.user , password=args.ocpassword ,  annotators=annString, base_url='https://run.opencravat.org')
+    run_opencravat(input_file=args.input_filename, username=args.user , password=args.ocpassword , assembly=args.assembly,  annotators=annString, base_url='https://run.opencravat.org')
 
 
 
